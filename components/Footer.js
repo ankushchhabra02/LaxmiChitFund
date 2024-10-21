10745d9a88f08394ceb7b634ddcbc2053ce737ba
Ankush Chhabra <ankushchhabra2002@gmail.com>
2024-10-22 00:13:52 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white flex items-center justify-center px-4 h-16">
+      <p className="text-center">
+        Copyright &copy; Laxmi Chit Fund - All rights reserved!
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
