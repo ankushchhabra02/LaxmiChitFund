f2fdbd3c7e2b13f65096a3a15f137ef22a5dcf5f
Ankush Chhabra <ankushchhabra2002@gmail.com>
2025-03-04 22:21:14 +0530

    razorpay integration

--- actions/useractions.js
+++ actions/useractions.js
@@ -1,36 +1,39 @@
-// "use server";
+"use server";
 
-// import Razorpay from "razorpay";
-// import Payment from "@/models/Payment";
-// import connectDb from "@/db/connectDb";
-// import User from "@/models/User";
+import Razorpay from "razorpay";
+import Payment from "@/models/Payment";
+import connectDb from "@/db/connectDb";
+import User from "@/models/User";
 
-// export const initiate = async (amount, to_username, paymentform) => {
-//   await connectDb();
-//   // fetch the secret of the user who is getting the payment
-//   let user = await User.findOne({ username: to_username });
-//   const secret = user.razorpaysecret;
+export const initiate = async (amount, to_username, paymentform) => {
+  await connectDb();
+  // fetch the secret of the user who is getting the payment
+  //   let user = await User.findOne({ username: to_username });
+  //   const secret = user.razorpaysecret;
 
-//   var instance = new Razorpay({ key_id: user.razorpayid, key_secret: secret });
+  var instance = new Razorpay({
+    key_id: process.env.KEY_ID,
+    key_secret: process.env.KEY_SECRET,
+  });
 
-//   let options = {
-//     amount: Number.parseInt(amount),
-//     currency: "INR",
-//   };
+  let options = {
+    amount: Number.parseInt(amount),
+    currency: "INR",
+  };
 
-//   let x = await instance.orders.create(options);
+  let x = await instance.orders.create(options);
 
-//   // create a payment object which shows a pending payment in the database
-//   await Payment.create({
-//     oid: x.id,
-//     amount: amount / 100,
-//     to_user: to_username,
-//     name: paymentform.name,
-//     message: paymentform.message,
-//   });
+  // create a payment object which shows a pending payment in the database
+  await Payment.create({
+    oid: x.id,
+    amount: amount / 100,
+    to_user: to_username,
+    name: paymentform.name,
+    message: paymentform.message,
+  });
 
-//   return x;
-// };
+  return x;
+};
 
 // export const fetchuser = async (username) => {
 //   await connectDb();
---
+++ components/PaymentPage.js
@@ -0,0 +1,212 @@
+"use client";
+import React, { useState } from "react";
+import Script from "next/script";
+import { initiate } from "@/actions/useractions";
+import { useSession } from "next-auth/react";
+
+const PaymentPage = ({ username }) => {
+  //   const { data: session } = useSession();
+
+  const [paymentform, setPaymentform] = useState({});
+
+  const handleChange = (e) => {
+    setPaymentform({ ...paymentform, [e.target.name]: e.target.value });
+  };
+
+  const pay = async (amount) => {
+    // get the order id
+    let a = await initiate(amount, username, paymentform);
+    let orderId = a.id;
+    var options = {
+      key: process.env.KEY_ID, // Enter the Key ID generated from the Dashboard
+      amount: amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      name: "Laxmi Chit Fund", //your business name
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      callback_url: `${process.env.URL}/api/razorpay`,
+      prefill: {
+        //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+        name: "Gaurav Kumar", //your customer's name
+        email: "gaurav.kumar@example.com",
+        contact: "9000090000", //Provide the customer's phone number for better conversion rates
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var rzp1 = new Razorpay(options);
+    rzp1.open();
+  };
+  return (
+    <>
+      <Script src="https://checkout.razorpay.com/v1/checkout.js"></Script>
+
+      <div className="cover w-full bg-red-50 relative">
+        <img
+          className="object-cover w-full h-[350]"
+          src="https://media.licdn.com/dms/image/v2/D4D16AQHQKVdNNCceZw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1682089688391?e=1743638400&v=beta&t=u_Dkn36HMqAvAyzooibw6h3cHUDFtbgXD3IJxMTB_Vk"
+          alt=""
+        />
+        <div className="absolute -bottom-20 right-[44%] border-white border-2 rounded-full ">
+          <img
+            className="rounded-full"
+            width={200}
+            height={200}
+            src="https://media.licdn.com/dms/image/v2/D4E35AQGBihbceVVI_w/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1719442247501?e=1741507200&v=beta&t=8OLVJjwGsZBAcdmSNbA4JQqxrtDmMpI5KI8dR4P07PM"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="info flex justify-center items-center my-24 mb-32 flex-col gap-2">
+        <div className="font-bold text-lg">@{username}</div>
+        <div className="text-slate-400">
+          Software Developer | Java | Next.js | Tailwind CSS | React Native
+        </div>
+        <div className="text-slate-400">628 followers . 500+ connections</div>
+        <div className="payment flex gap-3 w-[80%] mt-11">
+          <div className="supporters w-1/2 bg-slate-900 rounded-lg text-white p-10">
+            {/* Show list of all supporters as a list of leaderboard */}
+            <h2 className="text-2xl font-bold my-5">Supporters</h2>
+            <ul className="mx-5 text-lg">
+              <li className="my-4 flex gap-2 items-center">
+                <img
+                  className="border-none rounded-full"
+                  width={33}
+                  src="profile.gif"
+                  alt="user avatar"
+                />
+                <span>
+                  Ayush donated <span className="font-bold">₹20</span> with a
+                  message "Big Supporter"
+                </span>
+              </li>
+              <li className="my-4 flex gap-2 items-center">
+                <img
+                  className="border-none rounded-full"
+                  width={33}
+                  src="profile.gif"
+                  alt="user avatar"
+                />
+                <span>
+                  Ayush donated <span className="font-bold">₹20</span> with a
+                  message "Big Supporter"
+                </span>
+              </li>
+              <li className="my-4 flex gap-2 items-center">
+                <img
+                  className="border-none rounded-full"
+                  width={33}
+                  src="profile.gif"
+                  alt="user avatar"
+                />
+                <span>
+                  Ayush donated <span className="font-bold">₹20</span> with a
+                  message "Big Supporter"
+                </span>
+              </li>
+              <li className="my-4 flex gap-2 items-center">
+                <img
+                  className="border-none rounded-full"
+                  width={33}
+                  src="profile.gif"
+                  alt="user avatar"
+                />
+                <span>
+                  Ayush donated <span className="font-bold">₹20</span> with a
+                  message "Big Supporter"
+                </span>
+              </li>
+              <li className="my-4 flex gap-2 items-center">
+                <img
+                  className="border-none rounded-full"
+                  width={33}
+                  src="profile.gif"
+                  alt="user avatar"
+                />
+                <span>
+                  Ayush donated <span className="font-bold">₹20</span> with a
+                  message "Big Supporter"
+                </span>
+              </li>
+              <li className="my-4 flex gap-2 items-center">
+                <img
+                  className="border-none rounded-full"
+                  width={33}
+                  src="profile.gif"
+                  alt="user avatar"
+                />
+                <span>
+                  Ayush donated <span className="font-bold">₹20</span> with a
+                  message "Big Supporter"
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div className="makePayment w-1/2 bg-slate-900 rounded-lg text-white p-10">
+            <h2 className="text-2xl font-bold my-5">Make a Payment</h2>
+            <div className="flex gap-2 flex-col">
+              <input
+                onChange={handleChange}
+                value={paymentform.name}
+                name="name"
+                type="text"
+                className="w-full p-3 rounded-lg bg-slate-800"
+                placeholder="Enter Name"
+              />
+              <input
+                onChange={handleChange}
+                value={paymentform.message}
+                name="message"
+                type="text"
+                className="w-full p-3 rounded-lg bg-slate-800"
+                placeholder="Enter Message"
+              />
+              <input
+                onChange={handleChange}
+                value={paymentform.amount}
+                name="amount"
+                type="text"
+                className="w-full p-3 rounded-lg bg-slate-800"
+                placeholder="Enter Amount"
+              />
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Pay
+              </button>
+            </div>
+            {/* Or choose from these amounts */}
+            <div className="flex gap-2 mt-5">
+              <button
+                className="bg-slate-800 p-3 rounded-lg"
+                onClick={() => pay(1000)}
+              >
+                Pay ₹20
+              </button>
+              <button
+                className="bg-slate-800 p-3 rounded-lg"
+                onClick={() => pay(2000)}
+              >
+                Pay ₹50
+              </button>
+              <button
+                className="bg-slate-800 p-3 rounded-lg"
+                onClick={() => pay(3000)}
+              >
+                Pay ₹100
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PaymentPage;
