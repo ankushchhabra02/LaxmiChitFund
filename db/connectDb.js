caef4e63cc13ce4a0f037958927b835f21439f7a
Ankush Chhabra <ankushchhabra2002@gmail.com>
2025-04-10 14:45:22 +0530

    db

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,16 +1,28 @@
 import mongoose from "mongoose";
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      `mongodb://localhost:27017/laxmichitfund`,
-      {
-        useNewUrlParser: true,
-      }
-    );
-    console.log(`MongoDB Connected: {conn.connection.host}`);
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/laxmichitfund";
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  global.mongoose = cached;
+  return cached.conn;
+}
+
 export default connectDB;
